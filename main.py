568e75a266017ecb4f8d2f1585d1b816b77b42c1
pierregoaer <hello@pierregoaer.com>
2022-04-09 19:26:27 -0400

    Adding time selection with slider and copy to clipboard

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import pyperclip
 
 TIMER = 2
 
@@ -8,16 +9,33 @@ class Typist:
         self.window = Tk()
         self.window.title("Typist")
         self.window.config(padx=20, pady=20)
+
+        self.label = Label(text="Choose you duration in seconds:")
+        self.label.pack()
+
+        self.speed_slider = Scale(from_=1, to=120, orient='horizontal', command=self.get_timer)
+        self.speed_slider.set(20)
+        self.speed_slider.pack()
+
         self.text_box = Text()
-        self.text_box.pack()
+        self.text_box.pack(pady=20)
         self.text_box.bind("<KeyRelease>", self.reset_timer)
-        self.time_left = TIMER
+
+        self.copy_button = Button(text="Copy to clipboard", command=self.copy_to_clipboard)
+        self.copy_button.pack()
+
+        self.TIMER = 2
+        self.time_left = self.TIMER
         self.timer = None
+
+    def get_timer(self, event):
+        self.TIMER = self.speed_slider.get()
+        self.time_left = self.TIMER
 
     def reset_timer(self, event):
         if self.timer is not None:
             self.window.after_cancel(self.timer)
-            self.time_left = TIMER
+            self.time_left = self.TIMER
             self.countdown()
         else:
             self.countdown()
@@ -32,6 +50,11 @@ class Typist:
 
     def erase(self):
         self.text_box.delete('1.0', END)
+
+    def copy_to_clipboard(self):
+        text_to_copy = self.text_box.get("1.0", END)
+        pyperclip.copy(text_to_copy)
+        self.erase()
 
 
 typist = Typist()
